53ec99f387fddffff4809c6905f7c7ebc7de0ced
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2019-04-16 18:33:31 +0200

    Add common components
    * Text component
    *ÃTitle component

---
+++ front/src/views/commons/Text/index.js
@@ -0,0 +1,18 @@
+import styled, { css } from 'styled-components';
+import colors from '../../../constants/colors';
+
+const statusStyle = css`
+  display: inline-block;
+  margin-left: 10px;
+  color: black;
+`;
+
+const Text = styled.p`
+  color: ${({ danger }) => (danger ? colors.danger : 'initial')};
+  text-align: ${props => (props.center ? 'center' : 'left')};
+  font-size: ${props => (props.fontSize ? props.fontSize : 'medium')};
+  font-family: ${props => (props.font ? props.font : 'inherit')};
+  ${props => props.status && statusStyle};
+`;
+
+export default Text;
