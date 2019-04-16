cafc1fa25c37a90d298a8f12cfd5a2eaba5c288e
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2019-04-16 18:34:48 +0200

    Add secondary support.
    - secondary prop will give the Box component another box-shadow

--- front/src/views/commons/Box/index.js
+++ front/src/views/commons/Box/index.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 
 const Box = styled.div`
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  box-shadow: ${({ secondary }) =>
+    secondary
+      ? '0px 0px 40px -5px rgba(0, 0, 0, 0.11)'
+      : '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'};
 `;
 
 export default Box;
