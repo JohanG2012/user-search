dd3aadaf40c6783bacc796be6aec4431a02a4da0
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2019-04-21 04:56:44 +0200

    Add arrows for all directions

--- front/src/views/commons/Arrow/index.js
+++ front/src/views/commons/Arrow/index.js
@@ -1,12 +1,29 @@
-import styled from 'styled-components';
-import colors from '../../../constants/colors';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { UpArrow, DownArrow, RightArrow, LeftArrow } from './arrows';
 
-const Arrow = styled.div`
-  width: 0;
-  height: 0;
-  border-left: 20px solid transparent;
-  border-right: 20px solid transparent;
+const propTypes = {
+  up: PropTypes.bool,
+  down: PropTypes.bool,
+  left: PropTypes.bool,
+  right: PropTypes.bool,
+};
 
-  border-top: 20px solid ${colors.backgroundSecondary};
-`;
+const defaultProps = {
+  up: false,
+  down: false,
+  left: false,
+  right: false,
+};
+
+const Arrow = ({ up, down, left, right, ...rest }) => {
+  if (up) return <UpArrow {...rest} />;
+  if (down) return <DownArrow {...rest} />;
+  if (right) return <RightArrow {...rest} />;
+  if (left) return <LeftArrow {...rest} />;
+  return <DownArrow {...rest} />;
+};
+
+Arrow.defaultProps = defaultProps;
+Arrow.propTypes = propTypes;
 export default Arrow;
